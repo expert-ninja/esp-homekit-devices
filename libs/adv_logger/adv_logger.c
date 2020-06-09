eb5c52ad1ccf70f65c6cc28612ae15c163397bb5
Nick Altmann <nick.altmann@gmail.com>
2020-06-10 01:47:32 +0300

    Start logging from new line

--- libs/adv_logger/adv_logger.c
+++ libs/adv_logger/adv_logger.c
@@ -26,7 +26,7 @@
 #include <lwip/sockets.h>
 #include <stdout_redirect.h>
 
-#define HEADER_LEN                  (23)
+#define HEADER_LEN                  (24)
 #define UDP_LOG_LEN                 (2048 - HEADER_LEN)
 #define UDP_LOG_SEND_MIN_LEN        (UDP_LOG_LEN / 4)
 #define TRIES_BEFORE_FORCE_SEND     (20)
@@ -58,26 +58,26 @@ static ssize_t adv_logger_write(struct _reent *r, int fd, const void *ptr, size_
         if (((char *)ptr)[i] == '\r') {
             continue;
         }
-        
+
         if (((char *)ptr)[i] == '\n') {
             if (adv_logger_data->log_type >= 0) {
                 uart_putc(adv_logger_data->log_type, '\r');
             }
-            
+
             if (adv_logger_data->is_wifi_ready) {
                 adv_logger_data->udplogstring[adv_logger_data->udplogstring_len] = '\r';
                 adv_logger_data->udplogstring_len++;
             }
         }
-        
+
         if (adv_logger_data->log_type >= 0) {
             uart_putc(adv_logger_data->log_type, ((char *)ptr)[i]);
         }
-        
+
         if (adv_logger_data->is_wifi_ready && adv_logger_data->udplogstring_len < (UDP_LOG_LEN - HEADER_LEN)) {
             adv_logger_data->udplogstring[adv_logger_data->udplogstring_len] = ((char *)ptr)[i];
             adv_logger_data->udplogstring_len++;
-            
+
             if (((char *)ptr)[i] == '\n') {
                 adv_logger_data->udplogstring[adv_logger_data->udplogstring_len] = 0;
                 strcat(strcat(adv_logger_data->udplogstring, adv_logger_data->header), ": ");
@@ -85,7 +85,7 @@ static ssize_t adv_logger_write(struct _reent *r, int fd, const void *ptr, size_
             }
         }
     }
-    
+
     return len;
 }
 
@@ -98,31 +98,31 @@ static void adv_logger_task() {
     while (sdk_wifi_station_get_connect_status() != STATION_GOT_IP) {
         vTaskDelay(200 / portTICK_PERIOD_MS);
     }
-    
+
     if (sdk_wifi_get_ip_info(STATION_IF, &info)) {
         uint8_t macaddr[6];
         sdk_wifi_get_macaddr(STATION_IF, macaddr);
         snprintf(adv_logger_data->header, HEADER_LEN, IPSTR"-%02X%02X%02X", IP2STR(&info.ip), macaddr[3], macaddr[4], macaddr[5]);
-        adv_logger_data->udplogstring_len = snprintf(adv_logger_data->udplogstring, HEADER_LEN + 2, "%s-: ", adv_logger_data->header);
+        adv_logger_data->udplogstring_len = snprintf(adv_logger_data->udplogstring, HEADER_LEN + 4, "\n\n%s: ", adv_logger_data->header);
     } else {
         free(adv_logger_data->header);
         adv_logger_data->header = malloc(1);
         adv_logger_data->header[0] = 0;
         adv_logger_data->udplogstring_len = 0;
     }
-    
+
     adv_logger_data->is_wifi_ready = true;
-    
+
     lSocket = lwip_socket(AF_INET, SOCK_DGRAM, 0);
     memset((char *)&sLocalAddr, 0, sizeof(sLocalAddr));
     memset((char *)&sDestAddr, 0, sizeof(sDestAddr));
-    
+
     // Destination
     sDestAddr.sin_family = AF_INET;
     sDestAddr.sin_len = sizeof(sDestAddr);
     sDestAddr.sin_addr.s_addr = htonl(INADDR_BROADCAST);
     sDestAddr.sin_port = DESTINATION_PORT;
-    
+
     // Source
     sLocalAddr.sin_family = AF_INET;
     sLocalAddr.sin_len = sizeof(sLocalAddr);
@@ -135,13 +135,13 @@ static void adv_logger_task() {
             lwip_sendto(lSocket, adv_logger_data->udplogstring, adv_logger_data->udplogstring_len, 0, (struct sockaddr *)&sDestAddr, sizeof(sDestAddr));
             adv_logger_data->udplogstring_len = 0;
         }
-        
+
         if (i == TRIES_BEFORE_FORCE_SEND) {
             i = 0;
         }
-        
+
         i++;
-        
+
         vTaskDelay(20 / portTICK_PERIOD_MS); // With len > 1000 and delay = 20ms, we might handle 400kbps throughput
     }
 }
@@ -159,14 +159,14 @@ void adv_logger_init(const uint8_t log_type) {
         if (adv_logger_data->log_type > 1) {
             adv_logger_data->log_type -= 3;
         }
-        
+
         if (log_type > 2) {
             adv_logger_data->header = malloc(HEADER_LEN);
             adv_logger_data->udplogstring = malloc(UDP_LOG_LEN);
-            
+
             xTaskCreate(adv_logger_task, "adv_logger_task", ADV_LOGGER_TASK_SIZE, NULL, ADV_LOGGER_TASK_PRIORITY, NULL);
         }
-        
+
         set_write_stdout(adv_logger_write);
     }
 }
